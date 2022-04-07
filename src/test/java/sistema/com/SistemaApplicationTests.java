410ef08a655a4acb7ea486a65ff701494a539247
clau-p-calle <fer.calle123clau@gmail.com>
2022-04-06 20:15:42 -0500

    este es el inicio del sistema

---
+++ src/test/java/sistema/com/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
